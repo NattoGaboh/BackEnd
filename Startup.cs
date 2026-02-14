ac5dcd6408eed021beecbbf3ccab80052f54131a
Renato Vilquiniche <renato.vilquiniche+gysperu@gestionysistemas.com>
2026-02-13 19:53:28 -0500

    testing

--- Startup.cs
+++ Startup.cs
@@ -43,6 +43,7 @@ namespace BackEnd
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
